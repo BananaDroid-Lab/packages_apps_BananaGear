bananadroid/android_packages_apps_BananaGear

Gear: Extend DashboardFragment instead of SettingsPreferenceFragment
This makes us more inline with settings, adds toggle visibility animations and so on
Also add missing pages to search, correct wrong search xmls and more

Change-Id: I33fff0dcebf3234ce64d4b4eebcc4dfa56e2b997
Signed-off-by: Rizky Benggolo <travarilo@gmail.com>

src/com/banana/settings/fragments/DozeSettings.java
@@ -39,7 +39,7 @@ import androidx.preference.SwitchPreference;
 
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
-import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
@@ -53,7 +53,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 @SearchIndexable
-public class DozeSettings extends SettingsPreferenceFragment implements
+public class DozeSettings extends DashboardFragment implements
         Preference.OnPreferenceChangeListener {
 
     public static final String TAG = "DozeSettings";
@@ -89,10 +89,13 @@ public class DozeSettings extends SettingsPreferenceFragment implements
     private SharedPreferences mPreferences;
 
     @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.doze_settings;
+    }
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.doze_settings);
-
         Context context = getContext();
 
         PreferenceCategory dozeSensorCategory =
@@ -325,6 +328,11 @@ public class DozeSettings extends SettingsPreferenceFragment implements
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.BANANADROID;
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
     }
 
     /**

BananaDroid-Lab/packages_apps_BananaGear

src/com/banana/settings/fragments/DozeSettings.java
@@ -39,7 +39,7 @@ import androidx.preference.SwitchPreference;
 
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
-import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
@@ -50,7 +50,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 @SearchIndexable
-public class DozeSettings extends SettingsPreferenceFragment implements
+public class DozeSettings extends DashboardFragment implements
         Preference.OnPreferenceChangeListener {
 
     public static final String TAG = "DozeSettings";
@@ -79,10 +79,13 @@ public class DozeSettings extends SettingsPreferenceFragment implements
     private SharedPreferences mPreferences;
 
     @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.bg_doze;
+    }
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.bg_doze);
-
         Context context = getContext();
         ContentResolver resolver = context.getContentResolver();
 
@@ -273,6 +276,11 @@ public class DozeSettings extends SettingsPreferenceFragment implements
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.BANANADROID;
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
     }
 
     /**