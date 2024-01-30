bananadroid/android_packages_apps_BananaGear

Gear: Add toggle for about phone header style [2/3]
Change-Id: Ic862674da82a08b94028b8aaa74e2f758c210056

src/com/banana/settings/preferences/AboutHeaderPreferenceController.java
@@ -1,0 +1,63 @@
+/*
+ * Copyright (C) 2018 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.banana.settings.preferences;
+
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+import androidx.preference.SwitchPreference;
+import androidx.preference.Preference;
+
+import com.android.internal.util.banana.BananaUtils;
+import com.android.settings.DisplaySettings;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settings.R;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+import static android.provider.Settings.System.ABOUT_PHONE_HEADER;
+
+public class AboutHeaderPreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_ABOUT_PHONE_HEADER = "about_phone_header";
+
+    public AboutHeaderPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_ABOUT_PHONE_HEADER;
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        int aboutStyleToggleValue = Settings.System.getInt(mContext.getContentResolver(),
+                ABOUT_PHONE_HEADER, 0);
+        ((SwitchPreference) preference).setChecked(aboutStyleToggleValue != 0);
+    }
+
+    @Override
+    public boolean isAvailable() {
+	return true;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean aboutStyleToggleValue = (Boolean) newValue;
+        Settings.System.putInt(mContext.getContentResolver(), ABOUT_PHONE_HEADER, aboutStyleToggleValue ? 1 : 0);
+        BananaUtils.showSystemUiRestartDialog(mContext);
+        return true;
+    }
+}

BananaDroid-Lab/packages_apps_BananaGear

src/com/banana/settings/preferences/AboutHeaderPreferenceController.java
@@ -1,0 +1,63 @@
+/*
+ * Copyright (C) 2018 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.banana.settings.preferences;
+
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+import androidx.preference.SwitchPreference;
+import androidx.preference.Preference;
+
+import com.android.internal.util.banana.BananaUtils;
+import com.android.settings.DisplaySettings;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settings.R;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+import static android.provider.Settings.System.ABOUT_PHONE_HEADER;
+
+public class AboutHeaderPreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_ABOUT_PHONE_HEADER = "about_phone_header";
+
+    public AboutHeaderPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_ABOUT_PHONE_HEADER;
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        int aboutStyleToggleValue = Settings.System.getInt(mContext.getContentResolver(),
+                ABOUT_PHONE_HEADER, 0);
+        ((SwitchPreference) preference).setChecked(aboutStyleToggleValue != 0);
+    }
+
+    @Override
+    public boolean isAvailable() {
+	return true;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean aboutStyleToggleValue = (Boolean) newValue;
+        Settings.System.putInt(mContext.getContentResolver(), ABOUT_PHONE_HEADER, aboutStyleToggleValue ? 1 : 0);
+        BananaUtils.showSettingsRestartDialog(mContext);
+        return true;
+    }
+}