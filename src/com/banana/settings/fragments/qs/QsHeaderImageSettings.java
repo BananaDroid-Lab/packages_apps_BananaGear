250f3870f77f6739906e4ebd1be9f2b640407c3c
Rizky Benggolo <travarilo@gmail.com>
2024-02-19 10:38:19 +0700

    Gear: Improve Custom QS Header Image implementation
    Signed-off-by: Rizky Benggolo <travarilo@gmail.com>

--- src/com/banana/settings/fragments/qs/QsHeaderImageSettings.java
+++ src/com/banana/settings/fragments/qs/QsHeaderImageSettings.java
@@ -27,9 +27,12 @@ import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.content.res.Resources;
 import android.database.ContentObserver;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.UserHandle;
+import android.provider.MediaStore;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
 import android.view.View;
@@ -47,10 +50,19 @@ import com.android.settingslib.search.SearchIndexable;
 
 import com.banana.support.colorpicker.ColorPickerPreference;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import android.util.Log;
 
@@ -121,9 +133,9 @@ public class QsHeaderImageSettings extends SettingsPreferenceFragment
     @Override
     public boolean onPreferenceTreeClick(Preference preference) {
         if (preference == mQsHeaderImagePicker) {
-            Intent intent = new Intent(Intent.ACTION_PICK);
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
             intent.setType("image/*");
-            startActivityForResult(intent, REQUEST_IMAGE_PICKER);
+            startActivityForResult(intent, 10001);
             return true;
         }
         return super.onPreferenceTreeClick(preference);
@@ -195,17 +207,254 @@ public class QsHeaderImageSettings extends SettingsPreferenceFragment
     public void onActivityResult(int requestCode, int resultCode, Intent result) {
         super.onActivityResult(requestCode, resultCode, result);
 
-        if (requestCode == REQUEST_IMAGE_PICKER && resultCode == Activity.RESULT_OK) {
-            ContentResolver resolver = getContext().getContentResolver();
+        if (requestCode == 10001) {
+            if (resultCode != Activity.RESULT_OK) {
+                return;
+            }
             final Uri imgUri = result.getData();
             if (imgUri != null) {
+                String savedImagePath = saveImageToInternalStorage(getContext(), imgUri);
+                if (savedImagePath != null) {
+                ContentResolver resolver = getContext().getContentResolver();
                 Settings.System.putIntForUser(resolver,
                         Settings.System.QS_HEADER_IMAGE, OPTION_CUSTOM_IMAGE_VALUE,
                         UserHandle.USER_CURRENT);
                 Settings.System.putStringForUser(resolver,
-                        Settings.System.QS_HEADER_IMAGE_URI, imgUri.toString(),
+                        Settings.System.QS_HEADER_IMAGE_URI, savedImagePath,
                         UserHandle.USER_CURRENT);
+                }
             }
+        }
+    }
+
+
+  18 changes: 18 additions & 0 deletions18  
+res/values/strings.xml
+@@ -756,4 +756,22 @@
+    <string name="statusbar_top_padding">Top padding</string>
+    <string name="statusbar_left_padding">Left padding</string>
+    <string name="statusbar_right_padding">Right padding</string>
+
+    <!-- QS Header -->
+    <string name="qs_header_image_title">QS header image</string>
+    <string name="qs_header_image_summary">Select QS background header image</string>
+    <string name="qs_header_random_image_title">QS Header Random Image</string>
+    <string name="qs_header_random_image_summary">Enable random QS header image</string>
+    <string name="qs_header_image_blur_level_title">QS header image blur effect level</string>
+    <string name="qs_header_image_blur_level_summary">Strength of the blur effect applied to the QS header image</string>
+    <string name="qs_header_image_fade_level_level_title">QS header image fade level</string>
+    <string name="qs_header_image_fade_level_level_summary">Fade level of the QS header image</string>
+    <string name="qs_header_image_opacity_level_title">QS header image opacity level</string>
+    <string name="qs_header_image_opacity_level_summary">Opacity level of the QS header image</string>
+    <string name="qs_header_image_filter_color_title">QS header image filter color</string>
+    <string name="qs_header_image_height_title">QS header image height</string>
+    <string name="qs_header_custom_image_uri_enabled_title">Custom QS header image</string>
+    <string name="qs_header_custom_image_uri_enabled_summary">Apply custom QS image header</string>
+    <string name="file_header_select_title">Select local image</string>
+    <string name="file_header_select_summary">Select image from storage</string>
+</resources>
+  7 changes: 7 additions & 0 deletions7  
+res/xml/bg_quicksettings.xml
+@@ -66,6 +66,13 @@
+    <PreferenceCategory
+        android:title="@string/interface_title">
+
+        <!-- QS Header image -->
+        <Preference
+            android:key="qs_header_image"
+            android:title="@string/qs_header_image_title"
+            android:summary="@string/qs_header_image_summary"
+            android:fragment="com.banana.settings.fragments.qs.QsHeaderImageSettings" />
+
+        <!-- QS Tiles -->
+        <Preference
+            android:key="qs_tiles"
+ 58 changes: 58 additions & 0 deletions58  
+res/xml/qs_header_image_settings.xml
+@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="utf-8"?>
+<PreferenceScreen
+    android:title="@string/qs_header_image_title"
+    xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:settings="http://schemas.android.com/apk/res/com.android.settings">
+
+     <com.banana.support.preferences.SystemSettingSeekBarPreference
+        android:key="qs_header_image"
+        android:title="@string/qs_header_image_title"
+        android:defaultValue="0"
+        settings:interval="1"
+        android:max="24"
+        android:min="0" />
+
+    <com.banana.support.preferences.SystemSettingSwitchPreference
+        android:key="qs_header_custom_image_uri_enabled"
+        android:title="@string/qs_header_custom_image_uri_enabled_title"
+        android:summary="@string/qs_header_custom_image_uri_enabled_summary" />
+
+    <Preference
+        android:key="qs_header_custom_image_uri"
+        android:title="@string/file_header_select_title"
+        android:summary="@string/file_header_select_summary"
+        android:persistent="false"
+        android:dependency="qs_header_custom_image_uri_enabled" />
+
+    <com.banana.support.colorpicker.ColorPickerPreference
+        android:key="qs_header_image_filter_color"
+        android:title="@string/qs_header_image_filter_color_title"
+        android:defaultValue="0xFFFFFFFF" />
+
+    <com.banana.support.preferences.SystemSettingSeekBarPreference
+        android:key="qs_header_image_fade_level"
+        android:title="@string/qs_header_image_fade_level_level_title"
+        android:summary="@string/qs_header_image_fade_level_level_summary"
+        android:defaultValue="40"
+        settings:interval="1"
+        android:max="100"
+        android:min="1" />
+
+    <com.banana.support.preferences.SystemSettingSeekBarPreference
+        android:key="qs_header_image_height"
+        android:title="@string/qs_header_image_height_title"
+        android:defaultValue="80"
+        settings:interval="1"
+        android:max="255"
+        android:min="1" />
+
+    <com.banana.support.preferences.SystemSettingSeekBarPreference
+        android:key="qs_header_image_opacity_level"
+        android:title="@string/qs_header_image_opacity_level_title"
+        android:summary="@string/qs_header_image_opacity_level_summary"
+        android:defaultValue="30"
+        settings:interval="1"
+        android:max="100"
+        android:min="1" />
+
+</PreferenceScreen>
+ 138 changes: 138 additions & 0 deletions138  
+src/com/banana/settings/fragments/qs/QsHeaderImageSettings.java
+@@ -0,0 +1,138 @@
+package com.banana.settings.fragments.qs;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.MediaStore;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+import android.view.View;
+
+import androidx.preference.Preference;
+import androidx.preference.ListPreference;
+import androidx.preference.SwitchPreference;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
+
+import com.banana.support.colorpicker.ColorPickerPreference;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+@SearchIndexable
+public class QsHeaderImageSettings extends SettingsPreferenceFragment
+             implements Preference.OnPreferenceChangeListener{
+
+    private static final String KEY_CUSTOM_QS_HEADER_IMAGE_URI = "qs_header_custom_image_uri";
+
+    private Preference mQsHeaderCustomImagePicker;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.qs_header_image_settings);
+
+        mQsHeaderCustomImagePicker = findPreference(KEY_CUSTOM_QS_HEADER_IMAGE_URI);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        if (preference == mQsHeaderCustomImagePicker) {
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            intent.setType("image/*");
+            startActivityForResult(intent, 10001);
+            return true;
+        }
+        return super.onPreferenceTreeClick(preference);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent result) {
+        if (requestCode == 10001) {
+            if (resultCode != Activity.RESULT_OK) {
+                return;
+            }
+
+            final Uri imgUri = result.getData();
+            if (imgUri != null) {
+                String savedImagePath = saveImageToInternalStorage(getContext(), imgUri);
+                if (savedImagePath != null) {
+                    ContentResolver resolver = getContext().getContentResolver();
+                    Settings.System.putIntForUser(resolver, Settings.System.QS_HEADER_IMAGE, 0, UserHandle.USER_CURRENT);
+                    Settings.System.putStringForUser(resolver, Settings.System.QS_HEADER_CUSTOM_IMAGE_URI, savedImagePath, UserHandle.USER_CURRENT);
+                }
+            }
+        }
+    }
+
+    private String saveImageToInternalStorage(Context context, Uri imgUri) {
+        try {
+            InputStream inputStream;
+            if (imgUri.toString().startsWith("content://com.google.android.apps.photos.contentprovider")) {
+                List<String> segments = imgUri.getPathSegments();
+                if (segments.size() > 2) {
+                    String mediaUriString = URLDecoder.decode(segments.get(2), StandardCharsets.UTF_8.name());
+                    Uri mediaUri = Uri.parse(mediaUriString);
+                    inputStream = context.getContentResolver().openInputStream(mediaUri);
+                } else {
+                    throw new FileNotFoundException("Failed to parse Google Photos content URI");
+                }
+            } else {
+                inputStream = context.getContentResolver().openInputStream(imgUri);
+            }
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
+            String imageFileName = "QS_HEADER_" + timeStamp + ".png";
+            File directory = new File("/sdcard/qsheaders");
+            if (!directory.exists() && !directory.mkdirs()) {
+                return null;
+            }
+            File[] files = directory.listFiles((dir, name) -> name.startsWith("QS_HEADER_") && name.endsWith(".png"));
+            if (files != null) {
+                for (File file : files) {
+                    file.delete();
+                }
+            }
+            File file = new File(directory, imageFileName);
+            try (FileOutputStream outputStream = new FileOutputStream(file)) {
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+            }
+            return file.getAbsolutePath();
+        } catch (Exception e) {
+            return null;
         }
     }
 
